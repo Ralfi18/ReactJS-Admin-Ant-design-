732ebd96d9bb1edb1f3f8803526e9d65e9aef660
Rali Dimitrov <r.dimitrov.work@gmail.com>
2022-01-05 16:47:02 +0200

    Revert "first commit"
    This reverts commit c4352050e0bacca544f08f8cf9dae4b8af7fa0a6.

--- src/App.js
+++ src/App.js
@@ -1,41 +1,23 @@
-import React, { useEffect } from "react"
-import { Routes, BrowserRouter as Router, Route, Link, Outlet } from "react-router-dom";
-import io from "socket.io-client";
-import { AuthProvider, RequireAuth, useAuth, UserConsumer } from "./Auth";
-import LoginPage from "./LoginPage";
-import MainLayout from "./Layout";
-
-
-function Home() {
-  return(<h1>Home</h1>)
-}
-const About = () => (<h1>Test</h1>);
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  // useEffect(() => {
-    
-  //   const URL = "http://localhost:8080";
-  //   const socket = io(URL, { autoConnect: true });
-  //   console.log(socket)
-  //   socket.emit('chat message', "ping");
-  //   socket.on("connect", () => {  console.log(socket.id); });
-  //   socket.on("disconnect", () => {  console.log(socket.id + " disconnected"); });
-  // }, []); // Only re-run the effect if count changes
-  // const user = localStorage.getItem('user');
   return (
     <div className="App">
-      <AuthProvider>
-      <Router>
-        <Routes>
-          <Route element={<RequireAuth><MainLayout /></RequireAuth>}>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-          </Route>
-          <Route path="/login" element={<LoginPage />} /> 
-          {/* <Route path="about" element={<About />} /> */}
-        </Routes>
-      </Router>  
-      </AuthProvider>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
