c26260443b34a2424a8a7df1c4f8885917fe4f82
Rali Dimitrov <r.dimitrov.work@gmail.com>
2022-03-14 14:55:18 +0200

    refacturing

--- src/App.js
+++ src/App.js
@@ -1,139 +1,16 @@
-import React, { useContext, useState, useEffect } from "react"
-import { Routes, BrowserRouter as Router, Route, Link, Outlet } from "react-router-dom";
-import io from "socket.io-client";
+import React from "react"
+import { Routes, BrowserRouter as Router, Route } from "react-router-dom";
+// import io from "socket.io-client";
 import { AuthProvider, RequireAuth, RequireNotAuth } from "./Auth";
-import LoginPage from "./LoginPage";
-import MainLayout from "./Layout";
+import LoginPage from "./components/LoginPage";
+import MainLayout from "./components/Layout";
+import Home from "./components/Home";
+import Inventory from "./components/Inventory";
 import { connect } from 'react-redux';
-import { SET_SOCKET, UPDATE_INVENTORY } from "./store/types";
-import { AuthContext, useAuth } from "./Auth";
-import { Table, Space, Modal, Form, Input, InputNumber } from 'antd';
-
-function Home({ user }) {
-	const [message, setMessage] = useState("");
-	const context = useContext(AuthContext)
-
-	function handleSubmit(e) {
-		e.preventDefault();
-		context.socket.emit("message", { token: user.data.token, msg: message });
-	}
-
-	return (
-		<>
-			<form onSubmit={handleSubmit}>
-				<input type="text" onChange={(event) => setMessage(event.target.value)} />
-				<input type="submit" value="Submit" />
-			</form>
-		</>
-	)
-}
-
-const Inventory = ({ inventory, dispatch }) => {
-	// console.log(inventory)
-	const [isModalVisible, setIsModalVisible] = useState(false);
-	const [selectedInventory, setSelectedInventory] = useState(null);
-	const context = useContext(AuthContext);
-	const columns = [
-		{
-			title: 'ID',
-			dataIndex: 'id',
-			key: 'id',
-			// render: text => <a>{text}</a>
-		},
-		{
-			title: 'Name',
-			dataIndex: 'name',
-			key: 'name',
-		},
-		{
-			title: 'Price',
-			dataIndex: 'price',
-			key: 'price',
-			render: value => {
-				const prc = parseFloat(value).toLocaleString('gb-GB', { style: 'currency', currency: 'GBP' }); 
-
-				return <span><b>{prc}</b></span>;
-			}
-		},	
-		{
-			title: 'Action',
-			key: 'action',
-			render: (text, record) => (
-				<Space size="middle">
-					<a onClick={() => showModal(record)}>Edit</a>
-					<a>Delete</a>
-				</Space>
-			),
-		},	
-	];
-	const showModal = (record) => {
-		setSelectedInventory(record);
-		setIsModalVisible(true);
-	};
-
-	const handleOk = () => {
-		context.socket.emit("updateProduct", { token: context.user.data.token, product: selectedInventory });
-		// dispatch({ type: UPDATE_INVENTORY, payload: selectedInventory });
-		setSelectedInventory(null);
-		setIsModalVisible(false);
-	};
-
-	const handleCancel = () => {
-		setSelectedInventory(null);
-		setIsModalVisible(false);
-	};
-
-	const changeHandler = (value, name) => {
-		console.log(value, name)
-		console.log(value)
-	}; 
-
-	const pagination = {
-      // current: 1,
-      pageSize: 10,
-    };
-	return (
-		<>
-			<h1>Inventory Page</h1>
-			<Table pagination={pagination} columns={columns} dataSource={inventory.data} rowKey={(record) => record.code } />
-			<Modal title="Modal" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
-				{
-					selectedInventory ?
-					<>
-						<Form
-							labelCol={{span: 4}}
-							wrapperCol={{span: 14}}
-							layout="vertical"			
-						>
-							<Form.Item label="Product name">
-								<Input name="name" onChange={(e) => changeHandler(e.target.value, 'name')} defaultValue="" value={selectedInventory.name} />
-							</Form.Item>
-							<Form.Item label="Price">
-								<InputNumber 
-									name="price" 
-									onChange={(value) => changeHandler(value, 'price')} 
-									onStep={(value, info) => {
-								
-										const price = info.type == "up" ? parseFloat((selectedInventory.price + 0.01).toFixed(2)) : parseFloat((selectedInventory.price - 0.01).toFixed(2));
-										const tmpInv = {...selectedInventory, price}
-										setSelectedInventory(tmpInv)
-									}}
-									defaultValue="" 
-									value={selectedInventory.price} 
-								/>
-							</Form.Item>
-						</Form>
-					</>
-					:
-					<>No inventory selected</>
-				}
-			</Modal>
-		</>
-	)
-};
+// import { SET_SOCKET, UPDATE_INVENTORY } from "./store/types";
+// import { AuthContext, useAuth } from "./Auth";
 
 function App({ user, inventory, dispatch }) {
-
 	return (
 		<div className="App">
 			<AuthProvider loggedUser={user} dispatch={dispatch} >
---
+++ src/components/Layout.js
@@ -0,0 +1,84 @@
+import React, { useEffect, useState, useContext } from "react"
+import { Link, Outlet } from "react-router-dom"
+import { connect } from 'react-redux';
+import './App.css';
+import { AuthContext } from "../Auth";
+import {
+	useNavigate,
+	useLocation
+} from "react-router-dom";
+import { Layout, Menu, Alert } from 'antd';
+import {
+	MenuUnfoldOutlined,
+	MenuFoldOutlined,
+	UserOutlined,
+	VideoCameraOutlined,
+	UploadOutlined,
+} from '@ant-design/icons';
+import { LOGOUT_USER, DELETE_SOCKET, CLEAR_INVENTORY } from "../store/types";
+
+const { Header, Sider, Content } = Layout;
+function MainLayout({ dispatch }) {
+	const [collapsed, setCollapsed] = useState(false);
+	const context = useContext(AuthContext)
+	const navigate = useNavigate();
+	const location = useLocation();
+	return <Layout style={{ minHeight: '100vh' }}>
+		<Sider collapsible collapsed={collapsed} onCollapse={() => setCollapsed(!collapsed)} >
+			<div className="logo" />
+			<Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+				<Menu.Item key="1" icon={<UserOutlined />}>
+					<Link to="/">Home Page</Link>
+				</Menu.Item>
+				<Menu.Item key="2" icon={<VideoCameraOutlined />}>
+					<Link to="/inventory">Inventory</Link>
+				</Menu.Item>
+				<Menu.Item key="3" icon={<VideoCameraOutlined />} onClick={() => {
+					// Logout
+					context.setErrors(null);
+					context.signOut(() => {
+						dispatch({ type: LOGOUT_USER, payload: null });
+						dispatch({ type: DELETE_SOCKET, payload: null });
+						dispatch({ type: CLEAR_INVENTORY, payload: null });
+						navigate("login", { replace: true });
+					});
+				}} >
+					Logout
+				</Menu.Item>
+			</Menu>
+		</Sider>
+		<Layout className="site-layout">
+			<Header className="site-layout-background" style={{ padding: 0 }}>
+				{
+					context.offline === true ?
+					<Alert
+						message="Warning!!! You are working offline!!!"
+						type="error"
+					/>
+					:
+					null
+				}
+			</Header>
+			<Content
+				className="site-layout-background"
+				style={{
+					margin: '24px 16px',
+					padding: 24,
+					minHeight: 280,
+				}}
+			>
+				<Outlet />
+			</Content>
+		</Layout>
+
+	</Layout>
+}
+const mapStateToProps = (state) => {
+	return { user: state.userReducer.user };
+}
+
+const mapDispatchToProps = (dispatch) => {
+	return { dispatch };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(MainLayout);
